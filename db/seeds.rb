12ae1ae607b1b4426b2fcaa1849c39f63f14e1a5
valya <val.slyun@gmail.com>
2017-04-10 11:03:00 +0300

    added first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+first_user = User.create(name: 'Valentina', email: 'val.slyun@gmail.com', age: 25 ,family_name: 'Slyunyaeva',phone_number: '0919983948')
