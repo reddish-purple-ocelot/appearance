78d4500cacdef7c04296a310a5b53a68def93d88
valya <val.slyun@gmail.com>
2017-04-11 10:52:46 +0300

    added previews for mailers

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/foo
-  def foo
-    UserMailer.foo
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/foo_preview
+  def foo_preview
+    UserMailer.foo(User.first)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/bar
-  def bar
-    UserMailer.bar
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/bar_preview
+  def bar_preview
+    UserMailer.bar(User.first)
   end
 
 end
